4cc2cef4ddc876e08f0ac144b10f3e2e4b56bedb
Loïc Chardonnet <loic.chardonnet@gmail.com>
2014-05-05 21:45:52 +0200

    [Tests] Added Behat to test script

--- bin/tester.sh
+++ bin/tester.sh
@@ -4,3 +4,6 @@ redaktilo="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 echo '[phpspec] Running specification tests'
 $redaktilo/vendor/bin/phpspec run
+
+echo '[behat] Running story tests'
+$redaktilo/vendor/bin/behat
