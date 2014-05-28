8f65c28ebb17efff0e5cc4711990ab8bb8dbb2c2
Wouter J <waldio.webdesign@gmail.com>
2014-05-28 12:07:46 +0200

    [#11] Correct exit status code

--- bin/tester.sh
+++ bin/tester.sh
@@ -5,5 +5,15 @@ redaktilo="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 echo '[phpspec] Running specification tests'
 $redaktilo/vendor/bin/phpspec run
 
+phpspec=$?
+
 echo '[phpunit] Running example tests'
 $redaktilo/vendor/bin/phpunit
+
+phpunit=$?
+
+if [ $phpspec -eq 0 ] && [ $phpunit -eq 0]; then
+    exit 0
+else
+    exit 1
+fi
