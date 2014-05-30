24ffc67c6d1bc2800a9a94c2a9a6cadfde330b63
Loïc Chardonnet <loic.chardonnet@gmail.com>
2014-05-30 07:39:59 +0200

    [Substr] Created SubstringSearchStrategy

---
+++ src/Gnugat/Redaktilo/Search/SubstringSearchStrategy.php
@@ -0,0 +1,124 @@
+<?php
+
+/*
+ * This file is part of the Redaktilo project.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gnugat\Redaktilo\Search;
+
+use Gnugat\Redaktilo\Converter\LineContentConverter;
+use Gnugat\Redaktilo\File;
+
+/**
+ * This strategy manipulates lines stripped of their line break character.
+ *
+ * The match is done on the given substring.
+ *
+ * @api
+ */
+class SubstringSearchStrategy implements SearchStrategy
+{
+    /** @var LineContentConverter */
+    private $converter;
+
+    /** @param LineContentConverter */
+    public function __construct(LineContentConverter $converter)
+    {
+        $this->converter = $converter;
+    }
+
+    /**
+     * Checks if the given substring is present at least once in the file.
+     *
+     * @param File   $file
+     * @param string $pattern
+     *
+     * @return bool
+     *
+     * @api
+     */
+    public function has(File $file, $pattern)
+    {
+        $lines = $this->converter->from($file);
+        foreach ($lines as $line) {
+            if (false !== strpos($line, $pattern)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns the number of the given line if it is present after the current
+     * one.
+     *
+     * @param File   $file
+     * @param string $pattern
+     *
+     * @return integer
+     *
+     * @throws PatternNotFoundException If the line hasn't be found after the current one
+     */
+    public function findNext(File $file, $pattern)
+    {
+        $lines = $this->converter->from($file);
+        $currentLineNumber = $file->getCurrentLineNumber() + 1;
+        $nextLines = array_slice($lines, $currentLineNumber, null, true);
+        foreach ($nextLines as $lineNumber => $line) {
+            if (false !== strpos($line, $pattern)) {
+                return $lineNumber;
+            }
+        }
+
+        throw new PatternNotFoundException($file, $pattern);
+    }
+
+    /**
+     * Returns the number of the given line if it is present before the current
+     * one.
+     *
+     * @param File   $file
+     * @param string $pattern
+     *
+     * @return integer
+     *
+     * @throws PatternNotFoundException If the line hasn't be found before the current one
+     */
+    public function findPrevious(File $file, $pattern)
+    {
+        $lines = $this->converter->from($file);
+        $currentLineNumber = $file->getCurrentLineNumber() - 1;
+        $previousLines = array_slice($lines, 0, $currentLineNumber, true);
+        $reversedPreviousLines = array_reverse($previousLines, true);
+        foreach ($reversedPreviousLines as $lineNumber => $line) {
+            if (false !== strpos($line, $pattern)) {
+                return $lineNumber;
+            }
+        }
+
+        throw new PatternNotFoundException($file, $pattern);
+    }
+
+    /**
+     * @param mixed $pattern
+     *
+     * @return bool
+     *
+     * @api
+     */
+    public function supports($pattern)
+    {
+        if (!is_string($pattern)) {
+            return false;
+        }
+        $hasNoLineBreak = (false === strpos($pattern, "\n"));
+
+        return $hasNoLineBreak;
+    }
+}
---
+++ tests/spec/Gnugat/Redaktilo/Search/SubstringSearchStrategySpec.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * This file is part of the Redaktilo project.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Gnugat\Redaktilo\Search;
+
+use Gnugat\Redaktilo\Converter\LineContentConverter;
+use Gnugat\Redaktilo\File;
+use PhpSpec\ObjectBehavior;
+
+class SubstringSearchStrategySpec extends ObjectBehavior
+{
+    const FILENAME = '%s/tests/fixtures/sources/life-of-brian.txt';
+
+    function let(File $file, LineContentConverter $converter)
+    {
+        $rootPath = __DIR__.'/../../../../../';
+
+        $filename = sprintf(self::FILENAME, $rootPath);
+        $lines = file($filename, FILE_IGNORE_NEW_LINES);
+
+        $file->getFilename()->willReturn($filename);
+        $converter->from($file)->willReturn($lines);
+        $this->beConstructedWith($converter);
+    }
+
+    function it_is_a_search_strategy()
+    {
+        $this->shouldImplement('Gnugat\Redaktilo\Search\SearchStrategy');
+    }
+
+    function it_supports_lines()
+    {
+        $substring = 'why do witches burn?';
+        $substringWithLineBreak = "She turned me into a newt!\nA newt?\nI got better...";
+        $lineNumber = 42;
+
+        $this->supports($substring)->shouldBe(true);
+        $this->supports($substringWithLineBreak)->shouldBe(false);
+        $this->supports($lineNumber)->shouldBe(false);
+    }
+
+    function it_checks_line_presence(File $file)
+    {
+        $existingSubstring = '...Dickus?';
+        $nonExistingSubstring = 'Cornwall?';
+
+        $this->has($file, $existingSubstring)->shouldBe(true);
+        $this->has($file, $nonExistingSubstring)->shouldBe(false);
+    }
+
+    function it_finds_next_occurences(File $file)
+    {
+        $previousSubstring = 'sniggers';
+        $currentSubstring = 'More';
+        $currentLineNumber = 3;
+        $nextSubstring = 'Sniggering';
+        $nextLineNumber = 5;
+
+        $file->getCurrentLineNumber()->willReturn($currentLineNumber);
+
+        $exception = 'Gnugat\Redaktilo\Search\PatternNotFoundException';
+
+        $this->shouldThrow($exception)->duringFindNext($file, $previousSubstring);
+        $this->shouldThrow($exception)->duringFindNext($file, $currentSubstring);
+        $this->findNext($file, $nextSubstring)->shouldBe($nextLineNumber);
+    }
+
+    function it_finds_previous_occurences(File $file)
+    {
+        $previousSubstring = 'sniggers';
+        $previousLineNumber = 1;
+        $currentSubstring = 'More';
+        $currentLineNumber = 3;
+        $nextSubstring = 'Sniggering';
+
+        $file->getCurrentLineNumber()->willReturn($currentLineNumber);
+
+        $exception = 'Gnugat\Redaktilo\Search\PatternNotFoundException';
+
+        $this->shouldThrow($exception)->duringFindPrevious($file, $nextSubstring);
+        $this->shouldThrow($exception)->duringFindPrevious($file, $currentSubstring);
+        $this->findPrevious($file, $previousSubstring)->shouldBe($previousLineNumber);
+    }
+}
