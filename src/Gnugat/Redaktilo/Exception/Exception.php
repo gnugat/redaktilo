986e6cb2f00f00075c809ce422d096c5f250380f
Loick Piera <pyrech@gmail.com>
2014-10-22 23:29:36 +0200

    [Exception] Created an Exception interface

---
+++ src/Gnugat/Redaktilo/Exception/Exception.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the Redaktilo project.
+ *
+ * (c) Loïc Chardonnet <loic.chardonnet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gnugat\Redaktilo\Exception;
+
+interface Exception
+{
+}
